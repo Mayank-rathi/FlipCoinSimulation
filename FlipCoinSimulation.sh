f7e47c1af7623954417f7fa0992671e654dd55f9
Mayank-rathi <mayankrathi33@gmail.com>
2020-01-31 18:10:59 +0530

    ADDED:Singlet Combination in a Dictionary

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -x
 echo "Welcome To Flip Coin Simulation"
-Head=1;
-Tail=0;
+declare -A Singlet
 
-if [	$((RANDOM%2)) -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tails"
-fi
+#Variables
+head=0;
+tail=0;
+
+read -p "How Many Time You Want To Flip Coin: " i
+for((i=0;i<=10;i++))
+do
+	if [	$((RANDOM%2)) -eq 1 ]
+	then
+		Singlet[head]=$((++head))
+	else
+		Singlet[tail]=$((++tail))
+	fi
+done
+echo "Keys ${!Singlet[@]}"
+echo "count ${Singlet[@]}"
+echo	"Percentage of Head: `echo "scale=2; ${Singlet[head]}*100/$i" | bc`"%
+echo	"Percantage of Tail: `echo "scale=2; ${Singlet[tail]}*100/$i" | bc`"%
+
+
