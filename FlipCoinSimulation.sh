31aa4e1beb183febdef2080a4e124246bca42e71
Mayank-rathi <mayankrathi33@gmail.com>
2020-02-01 16:30:44 +0530

    ADDED:Store Doublet combination And it's percentage

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,24 +1,64 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome To Flip Coin Simulation"
-declare -A Singlet
+#Declaring A dictionary
+declare -A Combination
 
 #Variables
-head=0;
-tail=0;
+#!/bin/bash 
+declare -A coinsDictionary
+#Function to store flip count with respective coin sides
+function flipCoin(){
+	for(( i=1;i<=$1;i++ ))
+	do
+		side=""
+		for((j=1;j<=$2;j++))
+		do
+			randomValue=$((RANDOM%2))
+			if [ $randomValue -eq 1 ]
+			then
+				 side+=H
+			else
+				 side+=T
+			fi
+		done
+		updateCount $side
+	done
+}
 
-read -p "How Many Time You Want To Flip Coin: " n
-for((i=0;i<$n;i++))
-do
-	if [	$((RANDOM%2)) -eq 1 ]
-	then
-		Singlet[head]=$((++head))
-	else
-		Singlet[tail]=$((++tail))
-	fi
-done
-echo "Keys ${!Singlet[@]}"
-echo "count ${Singlet[@]}"
-echo	"Percentage of Head: `echo "scale=2; ${Singlet[head]}*100/$n" | bc`"%
-echo	"Percantage of Tail: `echo "scale=2; ${Singlet[tail]}*100/$n" | bc`"%
+function updateCount()
+{
+	#Storing the number of occurences with respect to side
+	coinsDictionary[$1]=$((${coinsDictionary[$1]}+1))
+}
+#Function to calculate percentage of occurence of each side
+function calculatePercentage()
+{
+	for i in ${!coinsDictionary[@]}
+	do
+		coinsDictionary[$i]=`echo "scale=2; ${coinsDictionary[$i]}*100/$noOfFlip" | bc`
+	done
+		echo "Key :	 	"${!coinsDictionary[@]}
+		echo "Percentage :	"${coinsDictionary[@]}
+}
 
 
+
+#User input for Number flip and fliping coin for singlet and doublet
+read -p "Enter how times you want to flip coin: " noOfFlip
+read -p "1)Singlet 2)Doublet Enter your choice: " choice
+#Case For Singlet and Doublet
+case $choice in
+	1)
+		noOfCoin=1
+		flipCoin $noOfFlip $noOfCoin
+		calculatePercentage
+		;;
+	2)
+		noOfCoin=2
+		flipCoin $noOfFlip $noOfCoin
+		calculatePercentage
+		;;
+	*)
+		echo "Invalid Choice"
+		;;
+esac
