a0f43d144b9bee6bc4e3f9559269b797f22873b0
Mayank-rathi <mayankrathi33@gmail.com>
2020-02-01 17:23:52 +0530

    ADDED:Sort Singlet Doublet and Triplet And Winning combination

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -3,37 +3,45 @@ echo "Welcome To Flip Coin Simulation"
 declare -A coinsDictionary
 #Function to store flip count with respective coin sides
 function flipCoin(){
-	for(( i=1;i<=$1;i++ ))
-	do
-		side=""
-		for((j=1;j<=$2;j++))
-		do
-			randomValue=$((RANDOM%2))
-			if [ $randomValue -eq 1 ]
-			then
-				 side+=H
-			else
-				 side+=T
-			fi
-		done
-		updateCount $side
-	done
+   for(( i=1;i<=$1;i++ ))
+   do
+      side=""
+      for((j=1;j<=$2;j++))
+      do
+         randomValue=$((RANDOM%2))
+         if [ $randomValue -eq 1 ]
+         then
+             side+=H
+         else
+             side+=T
+         fi
+      done
+      updateCount $side
+   done
 }
 
 function updateCount()
 {
-	#Storing the number of occurences with respect to side
-	coinsDictionary[$1]=$((${coinsDictionary[$1]}+1))
+   #Storing the number of occurences with respect to side
+   coinsDictionary[$1]=$((${coinsDictionary[$1]}+1))
 }
 #Function to calculate percentage of occurence of each side
 function calculatePercentage()
 {
-	for i in ${!coinsDictionary[@]}
-	do
-		coinsDictionary[$i]=`echo "scale=2; ${coinsDictionary[$i]}*100/$noOfFlip" | bc`
-	done
-		echo "Key :	 	"${!coinsDictionary[@]}
-		echo "Percentage :	"${coinsDictionary[@]}
+   for i in ${!coinsDictionary[@]}
+   do
+      coinsDictionary[$i]=`echo "scale=2; ${coinsDictionary[$i]}*100/$noOfFlip" | bc`
+   done
+      echo "Key :    "${!coinsDictionary[@]}
+      echo "Percentage :   "${coinsDictionary[@]}
+}
+#Fuction use For calculating Wining Percentage
+function winPercentage() {
+   echo "win Cobination Percentage"
+   for i in ${!coinsDictionary[@]}
+   do
+      echo "$i ${coinsDictionary[$i]}"
+   done | sort -k2 -rn | head -1
 }
 
 #User input for Number flip and fliping coin for singlet and doublet
@@ -41,22 +49,30 @@ read -p "Enter how times you want to flip coin: " noOfFlip
 read -p "1)Single Coin 2)Double Coin 3)Triple Coin Enter your choice: " choice
 #Case For Singlet and Doublet or Triplet
 case $choice in
-	1)
-		noOfCoin=1
-		flipCoin $noOfFlip $noOfCoin
-		calculatePercentage
-		;;
-	2)
-		noOfCoin=2
-		flipCoin $noOfFlip $noOfCoin
-		calculatePercentage
-		;;
-	3)
-		noOfCoin=3
-		flipCoin $noOfFlip $noOfCoin
-		calculatePercentage
-		;;
-	*)
-		echo "Invalid Choice"
-		;;
+   1)
+      noOfCoin=1
+      flipCoin $noOfFlip $noOfCoin
+      calculatePercentage
+      winPercentage
+      ;;
+   2)
+      noOfCoin=2
+      flipCoin $noOfFlip $noOfCoin
+      calculatePercentage
+      winPercentage
+      ;;
+   3)
+      noOfCoin=3
+      flipCoin $noOfFlip $noOfCoin
+      calculatePercentage
+      winPercentage
+      ;;
+   *)
+      echo "Invalid Choice"
+      ;;
 esac
+
+
+
+
+
